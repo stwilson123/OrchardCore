92bb001e90de124af61266b70958f94e84e85586
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-12-15 18:30:56 +0300

    Add users docs

--- src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/CreateUserContext.cs
+++ src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/CreateUserContext.cs
@@ -1,7 +1,14 @@
 namespace OrchardCore.Users.Handlers
 {
+    /// <summary>
+    /// Represents a <see cref="UserContextBase"/> for a user creation.
+    /// </summary>
     public class CreateUserContext : UserContextBase
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="CreateUserContext"/>.
+        /// </summary>
+        /// <param name="user">The <see cref="IUser"/>.</param>
         public CreateUserContext(IUser user) : base(user)
         {
         }
--- src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/IUserCreatedEventHandler.cs
+++ src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/IUserCreatedEventHandler.cs
@@ -2,8 +2,15 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Users.Handlers
 {
+    /// <summary>
+    /// Contract for user creation event
+    /// </summary>
     public interface IUserCreatedEventHandler
     {
+        /// <summary>
+        /// Occurs when the user is created.
+        /// </summary>
+        /// <param name="context">The <see cref="CreateUserContext"/>.</param>
         Task CreatedAsync(CreateUserContext context);
     }
 }
